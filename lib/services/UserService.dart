c58014e6a0c6f0805fb6754273c1e9d8dc6b07bf
Dipak Rana <dipakrana4608@gmail.com>
2022-07-18 06:10:47 +0530

    Local Database App in Flutter

---
+++ lib/services/UserService.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+
+import 'package:local_app/db_helper/repository.dart';
+import 'package:local_app/model/User.dart';
+
+class UserService {
+  late Repository moRepository;
+
+  UserService() {
+    moRepository = Repository();
+  }
+
+  //Save User
+  saveUser(User user) async {
+    return await moRepository.insertData('users', user.userMap());
+  }
+
+  //Read All Users
+  readAllUsers() async {
+    return await moRepository.readData('users');
+  }
+
+  //Edit User
+  updateUser(User user) async {
+    return await moRepository.updateData('users', user.userMap());
+  }
+
+  deleteUser(userId) async {
+    return await moRepository.deleteDataById('users', userId);
+  }
+  CheckEmailVerify(String email) async{
+    return await moRepository.checkEmail('users', email);
+  }
+}
